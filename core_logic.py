a8d4e6d880b0f868613811a1fb42e77895cc8bb3
abidmmp <abidmmp99@gmail.com>
2018-06-11 10:00:00 +0500

    Add missing return statement

--- core_logic.py
+++ core_logic.py
@@ -231,3 +231,4 @@
 // Handle None edge case - 2019-05-08 10:12:00
 // Add logging statements - 2020-08-03 10:08:00
 // Simplify conditional logic - 2021-11-03 10:04:00
+// Add missing return statement - 2018-06-11 10:00:00

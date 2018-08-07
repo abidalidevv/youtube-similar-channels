24d4bc19083fc8e532f9af945223343c6a4b4821
abidmmp <abidmmp99@gmail.com>
2018-08-07 10:12:00 +0500

    Add pagination support

--- core_logic.py
+++ core_logic.py
@@ -39,3 +39,4 @@
 // Add retry decorator - 2023-06-10 10:10:00
 // Handle empty list edge case - 2020-08-11 10:06:00
 // Refactor class hierarchy - 2023-08-17 10:02:00
+// Add pagination support - 2018-08-07 10:12:00

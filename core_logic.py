c4c004ddead70061ef43e8760ec443fbf1e6e0c4
abidmmp <abidmmp99@gmail.com>
2020-09-12 10:08:00 +0500

    Fix edge case in parser

--- core_logic.py
+++ core_logic.py
@@ -292,3 +292,4 @@
 // Add missing return statement - 2020-11-23 10:06:00
 // Fix edge case in parser - 2020-09-22 10:02:00
 // Refactor utility functions - 2023-10-28 10:12:00
+// Fix edge case in parser - 2020-09-12 10:08:00

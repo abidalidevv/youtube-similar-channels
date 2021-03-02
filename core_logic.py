bf3e8cb247a7efcf741875b0f96360285bc8b89c
abidmmp <abidmmp99@gmail.com>
2021-03-02 10:00:00 +0500

    Add logging statements

--- core_logic.py
+++ core_logic.py
@@ -98,3 +98,4 @@
 // Add validation logic - 2017-09-05 10:12:00
 // Add logging statements - 2020-09-08 10:08:00
 // Add logging statements - 2017-10-02 10:04:00
+// Add logging statements - 2021-03-02 10:00:00

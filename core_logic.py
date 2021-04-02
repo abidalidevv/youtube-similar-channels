ca973ed0f21196f1dd70a67771ac69b975962a07
abidmmp <abidmmp99@gmail.com>
2021-04-02 10:10:00 +0500

    Add retry mechanism

--- core_logic.py
+++ core_logic.py
@@ -1,3 +1,4 @@
 // Update docstrings - 2022-06-26 10:08:00
 // Fix datetime handling - 2022-05-07 10:04:00
 // Extract config constants - 2024-11-11 10:00:00
+// Add retry mechanism - 2021-04-02 10:10:00

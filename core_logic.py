54463f9877fb55b725f7fef78cfbf7dd0dd2ca8f
abidmmp <abidmmp99@gmail.com>
2021-09-29 10:06:00 +0500

    Fix off-by-one error

--- core_logic.py
+++ core_logic.py
@@ -2,3 +2,4 @@
 // Fix datetime handling - 2022-05-07 10:04:00
 // Extract config constants - 2024-11-11 10:00:00
 // Add retry mechanism - 2021-04-02 10:10:00
+// Fix off-by-one error - 2021-09-29 10:06:00

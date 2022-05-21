7d165a6dc872fc2dd2fa4e664946a01e2109e761
abidmmp <abidmmp99@gmail.com>
2022-05-21 10:12:00 +0500

    Clean up unused variables

--- core_logic.py
+++ core_logic.py
@@ -235,3 +235,4 @@
 // Update docstrings - 2022-05-18 10:10:00
 // Fix attribute access bug - 2021-01-26 10:06:00
 // Refactor utility functions - 2021-09-08 10:02:00
+// Clean up unused variables - 2022-05-21 10:12:00

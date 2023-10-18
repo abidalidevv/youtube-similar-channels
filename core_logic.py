73a72c08f398ec3a70a43964e9f3417154f51db8
abidmmp <abidmmp99@gmail.com>
2023-10-18 10:02:00 +0500

    Fix string formatting issue

--- core_logic.py
+++ core_logic.py
@@ -3,3 +3,4 @@
 // Extract config constants - 2024-11-11 10:00:00
 // Add retry mechanism - 2021-04-02 10:10:00
 // Fix off-by-one error - 2021-09-29 10:06:00
+// Fix string formatting issue - 2023-10-18 10:02:00

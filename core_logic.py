46ca720a17692639ede918efc0bdf45c073df146
Abid Ali <abidmmp99@gmail.com>
2025-06-25 11:31:05 +0000

    Add retry mechanism

--- core_logic.py
+++ core_logic.py
@@ -366,3 +366,6 @@ def get_env(key, default=''):
 
 def get_env(key, default=''):
     import os; return os.environ.get(key, default)
+
+def truncate(text, length=100, suffix='...'):
+    return text if len(text) <= length else text[:length-len(suffix)] + suffix

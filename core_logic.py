f34239f4a2a303d646f8610545d274a637645f8e
Abid Ali <abidmmp99@gmail.com>
2026-06-03 11:55:13 +0000

    Improve type hints

--- core_logic.py
+++ core_logic.py
@@ -500,3 +500,10 @@ def flatten(nested):
         if isinstance(item, list): result.extend(flatten(item))
         else: result.append(item)
     return result
+
+def memoize(fn):
+    cache = {}
+    def wrapper(*args):
+        if args not in cache: cache[args] = fn(*args)
+        return cache[args]
+    return wrapper
